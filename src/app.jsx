1963b8eb150e9de933ca63323c2c9c5ac0205ed0
IgosProjects <148572592+IgosProjects@users.noreply.github.com>
2024-03-19 18:54:46 +0100

    use react router instead of a custom one

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouteTasks } from './modules/routes';
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 function App() {
   return (
     <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
@@ -9,7 +13,7 @@ function App() {
         <div className='w-1/4 h-screen bg-orange-500 p-4' style={{ backgroundColor: 'rgba(255, 165, 0, 0.9)' }}> {/* Added inline style */}
           <h2 className='text-lg font-semibold mb-4'>Sidebar</h2>
           <div className='space-y-2'>
-            <button className='w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded' onClick={window.location = '/tasks'}>
+            <button className='w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded' onClick={() => { window.location = '/tasks' }}>
               Tasks
             </button>
             <button className='w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded' >
@@ -22,15 +26,22 @@ function App() {
         </div>
         {/* Main Content */}
         <div className='w-3/4 p-4'>
-  <RouteTasks />
-
+        <RouterProvider router={router} />
         </div>
       </div>
     </div>
   );
 }
-
-
+const router = createBrowserRouter([
+    {
+      path: "/tasks",
+      element: <Tasks />,
+    },
+    {
+        path: "/",
+        element: <Tasks />,
+      },
+  ]);
 function Tasks( ){
 return (
 <h1>fewffw</h1>
